e3b76a7df8eeaa455cb6d3b25fe52cc38f7c70ea
Sandeep Mantha <sandeep.mantha@anthem.com>
2019-05-06 09:30:11 -0400

    added license

---
+++ nimbus-core/src/main/java/com/antheminc/oss/nimbus/domain/model/state/queue/ParamStateMQEventListener.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2016-2018 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.antheminc.oss.nimbus.domain.model.state.queue;
+
+import com.antheminc.oss.nimbus.domain.model.state.EntityState;
+import com.antheminc.oss.nimbus.domain.model.state.EntityState.Param;
+import com.antheminc.oss.nimbus.domain.model.state.ModelEvent;
+import com.antheminc.oss.nimbus.integration.mq.ActiveMqPublisher;
+
+/**
+ * @author Sandeep Mantha
+ * 
+ */
+public class ParamStateMQEventListener extends ParamStateQueueEventListener {
+	
+	ActiveMqPublisher mqPublisher;
+
+	public ParamStateMQEventListener(ActiveMqPublisher mqPublisher) {
+		this.mqPublisher = mqPublisher;
+	}
+	
+	@Override
+	public boolean shouldAllow(EntityState<?> p) {
+		return super.shouldAllow(p);
+	}
+	
+	@Override
+	public boolean listen(ModelEvent<Param<?>> event) {
+		Param<?> p = (Param<?>) event.getPayload();
+		
+		this.mqPublisher.sendMessage(p);
+		return true;
+	}
+}
